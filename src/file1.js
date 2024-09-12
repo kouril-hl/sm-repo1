ff131d217b9be62ca842d022e07f8926e3a03363
David Kouřil <kouril@haug.land>
2024-09-12 08:09:47 +0200

    test2

--- src/file1.js
+++ src/file1.js
@@ -4,3 +4,4 @@ console.log('file1.js');
 console.log('file1.js branch1');
 console.log(trimStart('file1.js from repo1', 'file1.js'));
 console.log(trimStart('file1.js from repo1', 'file1.js'));
+console.log(trimStart('file1.js from repo1', 'file1.js'));
